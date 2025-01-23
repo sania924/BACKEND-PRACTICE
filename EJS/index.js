0f27c4c39582cfb1c0a1ff560959201922fab5c7
sania924 <saniayasin924@gmail.com>
2025-01-24 00:23:06 +0500

    EJS

--- EJS/index.js
+++ EJS/index.js
@@ -2,7 +2,26 @@ const express = require("express");
 const app = express();
 // console.dir(app);
 let port = 8080;
+const path = require("path");
+
 // for listen request
 app.listen(port, () => {
   console.log(`port is listen on ${port}`);
+});
+// for route
+app.get("/", (req, res) => {
+  res.send("this is home root");
+});
+// for view directory in root directory
+app.set("views", path.join(__dirname, "/views"));
+//for render html file
+app.get("/help", (req, res) => {
+  res.render("home.ejs");
+});
+
+// for instram ejs
+app.get("/ig/:username", (req, res) => {
+  let { username } = req.params;
+
+  res.render("instagram.ejs", { username });
 });
