044113d3ac4d3e9420a7446a6a120f659ca7fbd9
sania924 <saniayasin924@gmail.com>
2025-01-24 23:13:04 +0500

    ejs

--- EJS/index.js
+++ EJS/index.js
@@ -20,8 +20,20 @@ app.get("/help", (req, res) => {
 });
 
 // for instram ejs
+// app.get("/ig/:username", (req, res) => {
+//   let { username } = req.params;
+
+//   res.render("instagram.ejs", { username });
+// });
+
+// for create insta page use db.json
 app.get("/ig/:username", (req, res) => {
   let { username } = req.params;
-
-  res.render("instagram.ejs", { username });
+  const instadata = require("./data.json");
+  const data = instadata[username];
+  if (data) {
+    res.render("instagram.ejs", { data });
+  } else {
+    res.render("errors.ejs");
+  }
 });
